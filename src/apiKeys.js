2307cfa2e52641591ae0e3bb203994951a938aea
Souvik Maity <souvik1189.be21@chitkarauniveristy.edu.in>
2024-08-27 11:48:08 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "aba6ff9d6de967d5eac6fd79114693cc",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
